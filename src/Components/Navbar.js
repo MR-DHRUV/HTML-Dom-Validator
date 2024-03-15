65baa9a12abb3dce112290d582f18aaf05b65682
dhruv <dhruvgupta742@gmail.com>
2024-03-15 13:29:34 +0530

    fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 "use client";
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal';
-import Readme from './readme';
+import Readme from './Readme';
 
 const Navbar = ({ validate }) => {
 
